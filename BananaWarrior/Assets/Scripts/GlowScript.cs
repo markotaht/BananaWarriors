5ded385e4e066f55ad7c5cb982a6006ff7e5c429
SiiMeR <siimraudsepp@msn.com>
2017-04-22 18:58:24 +0300

    GoldenBanana elu alged

---
+++ BananaWarrior/Assets/Scripts/GlowScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowScript : MonoBehaviour {
+
+    [SerializeField]
+    private float maxGlow = 2f;
+
+    [SerializeField]
+    private float minGlow = 0.3f;
+
+    [SerializeField]
+    private float glowStep = 0.01f;
+    private bool isGrowing;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float currentHaloStrength = GetComponent<Light>().range;
+
+        if (isGrowing)
+        {
+            if(currentHaloStrength >= maxGlow)
+            {
+                isGrowing = !isGrowing;
+            }
+            else
+            {
+                GetComponent<Light>().range += glowStep;
+            }
+        }
+        else
+        {
+            if(currentHaloStrength <= minGlow)
+            {
+                isGrowing = !isGrowing;
+            }
+            else
+            {
+                GetComponent<Light>().range -= glowStep;
+            }
+        }
+        //RenderSettings.haloStrength += 0.01f;
+
+	}
+}
