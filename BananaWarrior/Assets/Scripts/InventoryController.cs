23213200533aa683e59077a3e8d8194bd7b8e6b9
marko <markotaht@gmail.com>
2017-04-22 13:27:30 +0300

    Collecting bananas

---
+++ BananaWarrior/Assets/Scripts/InventoryController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour {
+
+    private int greenBanana = 0;
+    private int yellowbanana = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "GreenBanana")
+        {
+            greenBanana += 1;
+        }
+        else if(collision.tag == "YellowBanana")
+        {
+            yellowbanana += 1;
+        }
+    }
+
+    public void useBananas()
+    {
+        //Midagi teha banaanidega
+    }
+}
