2c3063fc097772c69c66aa78a4c0255ebadcf3e1
SiiMeR <siimraudsepp@msn.com>
2017-04-23 09:33:39 +0300

    broken main menu shipped

---
+++ BananaWarrior/Assets/Scripts/MenuItemMouseHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuItemMouseHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        foreach (var obj in eventData.hovered) Debug.Log(obj);
+        //  {
+        ///       Debug.Log("on");
+        //       startcolor = text1.color;
+        //     text1.color = Color.red;
+        //  }
+        isOver = true;
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isOver = false;
+        text1.color = startcolor;
+    }
+
+}
