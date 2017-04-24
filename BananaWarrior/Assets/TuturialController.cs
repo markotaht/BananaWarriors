fa05f900aaec714fbcee318e0a1fbec467d0d40e
SiiMeR <siimraudsepp@msn.com>
2017-04-24 15:26:24 +0300

    tutorial fixes

---
+++ BananaWarrior/Assets/TuturialController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TuturialController : MonoBehaviour {
+
+    int currentTut = 0;
+    public Texture2D tut1;
+    public Texture2D tut2;
+    public Texture2D tut3;
+    public Texture2D tut4;
+    public Texture2D tut5;
+    public Texture2D tut6;
+
+    List<Texture2D> tuts;
+    // Use this for initialization
+    void Start () {
+        tuts = new List<Texture2D>();
+        tuts.Add(tut1);
+        tuts.Add(tut2);
+        tuts.Add(tut3);
+        tuts.Add(tut4);
+        tuts.Add(tut5);
+        tuts.Add(tut6);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.GetComponent<Image>().material = tuts[currentTut];
+	}
+
+    public void nextButton()
+    {
+        if (currentTut < 5) currentTut++;
+    }
+
+    public void previousButton()
+    {
+        if (currentTut > 0) currentTut--;
+    }
+}
